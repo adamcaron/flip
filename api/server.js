c4edb312107aab4013319be7fd844fe454d97e8e
Adam Caron <adamwcaron@gmail.com>
2017-04-28 15:15:50 -0600

    Adds server

---
+++ api/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8085
+const router = express.Router()
+router.get('/', function(req, res) {
+  res.json({ message: 'It\'s working' })
+})
+app.use('/api', router)
+app.listen(port)
+console.log('Server running on port ' + port)
