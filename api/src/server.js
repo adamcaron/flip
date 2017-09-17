f43802ff8f033ba93e4bee7f280452d76e965c5e
Adam Caron <adamwcaron@gmail.com>
2017-09-17 08:23:32 -0600

    Hook up listings

--- api/src/server.js
+++ api/src/server.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8085
 const router = express.Router()
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   res.json({ message: 'It\'s working' })
+})
+router.get('/listings', (req, res) => {
+  res.json({ message: 'Listings' })
 })
 app.use('/api/', router)
 app.listen(port)
---
+++ ui/src/components/Home.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+  listings
+</template>
+
+<script>
+import Listings from './Listings'
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      results: 'Click to retrieve results'
+    }
+  },
+  methods: {
+    getListings: function () {
+      this.$http
+        .get('/api/')
+        .then(response => {
+          this.results = response.data.message
+        })
+        .catch(error => {
+          console.error(error)
+          this.results = `Not able to retrieve listings`
+        })
+    }
+  },
+  components: {
+    Listings
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
